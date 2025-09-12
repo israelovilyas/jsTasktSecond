f4e22eb5bee4e2948dfafad94f3be21a7a0472bc
israelovilyas <israelovilyas@gmail.com>
2025-09-12 12:44:14 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let firstName = "ilyas";
+let lastName = "israyilov";
+let fullName = firstName + lastName;
+console.log(fullName.length);
+
+let num = 10;
+console.log(Math.pow(num, 5));
+console.log(num % 3);
