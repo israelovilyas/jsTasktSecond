8a3e37d4e23a725d3f4e94f6564cf1d8c976a2a1
israelovilyas <israelovilyas@gmail.com>
2025-09-15 02:24:28 +0400

    third task edition

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
-let firstName = "ilyas";
-let lastName = "israyilov";
-let fullName = firstName + lastName;
-console.log(fullName.length);
+// let firstName = "ilyas";
+// let lastName = "israyilov";
+// let fullName = firstName + lastName;
+// console.log(fullName.length);
 
-let num = 10;
-console.log(Math.pow(num, 5));
-console.log(num % 3);
+// let num = 10;
+// console.log(Math.pow(num, 5));
+// console.log(num % 3);
+
+let a = 12345;
+let b = 98765;
+let c = (a * b);
+console.log(String(c).charAt(5));
+
+let text = prompt("Enter your text");
+console.log(text.length);
+console.log(text.includes("e"));
+console.log(text.startsWith("code"));
+console.log(text.endsWith("code"));
+console.log(text.replaceAll("####"));
+
+
+
+
+
